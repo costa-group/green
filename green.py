f91f7d3d129ace6d5be9717c9a32c83aa8a1357f
Pablo Gordillo <tutugordillo@gmail.com>
2023-06-15 14:10:05 +0200

    main

---
+++ green.py
@@ -0,0 +1,8 @@
+import importlib
+
+ethir = importlib.import_module("ethir-complete.ethir")
+gasol = importlib.import_module("gasol-optimizer")
+
+
+if __name__ == "__main__":
+    print("Green Main")
