fe2223e9404151c9bae395cd1d32ee9a7f877f92
Pablo Gordillo <tutugordillo@gmail.com>
2023-06-19 13:26:40 +0200

    updating submodules

--- green.py
+++ green.py
@@ -1,8 +1,18 @@
+#python3
 import importlib
 
 ethir = importlib.import_module("ethir-complete.ethir")
 gasol = importlib.import_module("gasol-optimizer")
 
+import os
+import argparse
+import traceback
+import sys
+
 
 if __name__ == "__main__":
-    print("Green Main")
+    # import ethir.oyente_ethir as eth
+    # eth.analyze_solidity()
+    print("HOLA")
+
+    
